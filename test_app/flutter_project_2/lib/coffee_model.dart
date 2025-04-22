bccec15be8c1fd0ce07f7e8307ab4df4058451f2
ASSEL-AI <assel.almalki711@gmail.com>
2025-04-22 09:02:45 +0300

    first commit

---
+++ test_app/flutter_project_2/lib/coffee_model.dart
@@ -0,0 +1,13 @@
+class CoffeeModel {
+  final String name;
+  final String description;
+  final String price;
+  final String image;
+
+  CoffeeModel({
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.image,
+  });
+}
